0622b504a14bb87102b85335e038bf14d0ab060f
nkkoufo <nkkoufo@gmail.com>
2018-11-17 16:20:18 -0500

    Changed 497
    this file now generated responses to each event and writing them to file, for every event title it generates between 1 and 20 responses taking from a array that has pregenerated positive or negative phrases, 6 positive 2 neutral 6 negative and selecting randomly from this array

--- 497.py
+++ 497.py
@@ -4,11 +4,36 @@ Created on Sun Nov 11 16:11:02 2018
 
 @author: nkkou
 """
+#imports
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import random
 
-p = urlopen('https://news.psu.edu/campus/erie')
+#declarations
+url = 'https://news.psu.edu/campus/erie'
+titles = []
+generated = []
+responses = ["was Great!", "was amzaing", "was fun", "had a great vibe", "was a vibe", "meh",
+             "needed more music to be fun but was alright", "needed more music", "didnt have enough to do",
+             "didnt have enought people there", "was awful", "sucked", "wasn't fun"]
+
+#Grab titles and store in array
+p = urlopen(url)
 soup = BeautifulSoup(p,"lxml")
 soup.prettify
 for row in soup.find_all('h2',attrs={"class" : "node-title"}):
     print(row.text)
+    titles.append(row.text)
+for i in range(len(titles)):
+    x = random.randint(1,20)
+    for j in range(0,x):
+        r = random.randint(0,len(responses)-1)
+        print(r)
+        g = 'I thought ' + titles[i] + ' ' + responses[r]
+        generated.append(g)
+file = open("testdata.txt", "w")
+for i in range(len(generated)):
+    file.write(generated[i] + "\n")
+file.close
+    
+    
