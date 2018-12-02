d653441c8dc280ab0d6751b1cc6e8386073c1ed4
nkkoufo <nkkoufo@gmail.com>
2018-12-02 17:50:16 -0500

    added elements to array

--- SentimentAnalyzer.py
+++ SentimentAnalyzer.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sat Nov 17 15:48:22 2018
-
 @author: zcast7172
 """
+from collections import namedtuple
 import json
 from watson_developer_cloud import NaturalLanguageUnderstandingV1 as NLU
-from watson_developer_cloud.natural_language_understanding_v1 import Features,KeywordsOptions
+from watson_developer_cloud.natural_language_understanding_v1 import Features,KeywordsOptions,SentimentOptions
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import random
@@ -18,8 +18,30 @@ generated = []
 responses = ["was Great!", "was amazing", "was fun", "had a great vibe", "was a vibe", "meh",
              "needed more music to be fun but was alright", "needed more music", "didnt have enough to do",
              "didnt have enought people there", "was awful", "sucked", "wasn't fun"]
+responseArray = []
+class generatedElement(object):
+    sentence = ""
+    title = ""
+    
+class responseArrayElement(object):
+    sentence = ""
+    score = 0.0
+    title = ""
 
-#Grab titles and store in array
+def make_gE(sentence, title):
+    a = generatedElement()
+    a.sentence = sentence
+    a.title = title
+    return a
+
+def make_RAE(sentence, score, title):
+    a = responseArrayElement()
+    a.sentence = sentence
+    a.score = score
+    a.title = title
+    return a
+    
+#Grab titles create text and store in array
 p = urlopen(url)
 soup = BeautifulSoup(p,"lxml")
 soup.prettify
@@ -31,16 +53,25 @@ for i in range(len(titles)):
     for j in range(0,x):
         r = random.randint(0,len(responses)-1)
         print(r)
-        g = 'I thought ' + titles[i] + ' ' + responses[r]
+        g = make_gE('I thought ' + titles[i] + ' ' + responses[r], titles[i])
         generated.append(g)
 
-
+#sentiment analysis
 nlu = NLU(version='2018-03-16',
           iam_apikey='SXP-aQb8p1wxsCr3wY_9VVgoJGyXQISul6L4DjNsN4Di',
           url='https://gateway.watsonplatform.net/natural-language-understanding/api')
 
 for item in generated:
     response = nlu.analyze(
-            url = 'https://news.psu.edu/campus/erie',
-            features=Features(keywords=KeywordsOptions(sentiment=True,emotion=True,limit=2))).get_result()
+            text = item.sentence,
+            return_analyzed_text=True,
+            DocumentSentimentResults=True,
+            features=Features(
+            sentiment=SentimentOptions(document=True)
+            )).get_result()
     print(json.dumps(response, indent=2))
+    score = response["sentiment"]["document"]["score"]
+    text = response["analyzed_text"]
+    title = item.title
+    r = make_RAE(text, score, title)
+    responseArray.append(r)
